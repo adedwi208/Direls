56e8a24df3671c3c8fe132d9f061156b0f2cb790
adedwi208 <adeagustus02@gmail.com>
2025-09-02 18:14:32 +0700

    selesai

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: 3306, // ini fixed karena MySQL pakai 3306
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
 module.exports = db;
